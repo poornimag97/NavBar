b1360f72921fced9283eed9e0a511618d597a310
poornimag97 <126610347+poornimag97@users.noreply.github.com>
2023-03-06 11:48:11 +0530

    Add files via upload

---
+++ Logo.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+ const Logo=()=>{
+    return(
+        <div>
+            <img src="https://images.unidays.world/i/customers/mobile/active/e341a916-7c11-4c21-9d56-59eb3056dada" alt="" /> 
+            </div>   )
+ }
+ export default Logo
